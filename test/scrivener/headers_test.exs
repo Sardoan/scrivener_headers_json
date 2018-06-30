dff45755cdb85146834f7cefce6574cbac9d1273
Christoph Otreba <co@biqx.de>
2018-06-30 14:16:34 +0200

    Add scrivener_headers_json

---
+++ test/scrivener/headers_test.exs
@@ -0,0 +1,60 @@
+defmodule Scrivener.HeadersTests do
+  use ExUnit.Case, async: true
+
+  alias Plug.Conn
+  alias Scrivener.{Headers, Page}
+
+  defp paginated_headers(page, port \\ 80) do
+    conn = %Conn{host: "www.example.com",
+                 port: port,
+                 query_string: "foo=bar",
+                 request_path: "/test",
+                 scheme: :http}
+            |> Headers.paginate(page)
+
+    conn.resp_headers
+    |> Enum.into(%{})
+  end
+
+  test "add pagination headers" do
+    page = %Page{page_number: 3, page_size: 10, total_pages: 5, total_entries: 50}
+    headers = paginated_headers(page)
+
+    assert headers["total"] == "50"
+    assert headers["per-page"] == "10"
+    assert headers["total-pages"] == "5"
+    assert headers["page-number"] == "3"
+    links = Poison.decode!(headers["link"])
+    assert Enum.count(links) == 4
+  end
+
+  test "doesn't include prev link for first page" do
+    page = %Page{page_number: 1, page_size: 10, total_pages: 5, total_entries: 50}
+    headers = paginated_headers(page)
+    links = Poison.decode!(headers["link"])
+    link = Enum.find(links, fn(x)
+    -> Map.get(x, "prev", nil) != nil
+    end)
+    assert link == nil
+  end
+
+  test "doesn't include next link for last page" do
+    page = %Page{page_number: 5, page_size: 10, total_pages: 5, total_entries: 50}
+    headers = paginated_headers(page)
+    links = Poison.decode!(headers["link"])
+    link = Enum.find(links, fn(x)
+    -> Map.get(x, "next", nil) != nil
+    end)
+    assert link == nil
+  end
+
+  test "includes ports other than 80 and 443" do
+    page = %Page{page_number: 5, page_size: 10, total_pages: 5, total_entries: 50}
+    headers = paginated_headers(page, 1337)
+    links = Poison.decode!(headers["link"])
+    link = Enum.find(links, fn(x)
+      -> Map.get(x, "first", nil) != nil
+    end)
+    assert link["first"] == "http://www.example.com:1337/test?foo=bar&page=1"
+  end
+end
